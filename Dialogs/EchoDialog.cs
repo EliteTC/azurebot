73f49cfab326cde6e67e305720779da684b4a6d7
EliteTC <deadend1408@gmail.com>
2018-02-27 12:05:35 +0200

    initial commit

---
+++ Dialogs/EchoDialog.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+
+
+namespace Microsoft.Bot.Sample.SimpleEchoBot
+{
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // User message
+            string userMessage = activity.Text;
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    //Assuming that the api takes the user message as a query paramater
+                    string RequestURI = "https://jsonplaceholder.typicode.com/posts/" + userMessage;
+                    HttpResponseMessage responsemMsg = await client.GetAsync(RequestURI);
+                    if (responsemMsg.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await responsemMsg.Content.ReadAsStringAsync();
+
+                        //Post the API response to bot again
+                        await context.PostAsync($"Response is {apiResponse}");
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+    /*
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        protected int count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            if (message.Text == "reset")
+            {
+                PromptDialog.Confirm(
+                    context,
+                    AfterResetAsync,
+                    "Are you sure you want to reset the count?",
+                    "Didn't get that!",
+                    promptStyle: PromptStyle.Auto);
+            }
+            else
+            {
+                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+        */
+}
