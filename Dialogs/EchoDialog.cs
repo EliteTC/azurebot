5ab4c11ad60a50e7ddaf6774d0ab92a28ea9c218
EliteTC <deadend1408@gmail.com>
2018-03-20 21:51:18 +0200

    update

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -4,7 +4,8 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Net.Http;
-
+using System.Collections.Generic;
+using SimpleEchoBot.Controllers;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
@@ -17,26 +18,28 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
 
             return Task.CompletedTask;
         }
-
+        
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
 
             // User message
             string userMessage = activity.Text;
+           AdaptiveCards.AdaptiveCard card =  MessageHandler.GetCard(userMessage);
             try
             {
                 using (HttpClient client = new HttpClient())
                 {
                     //Assuming that the api takes the user message as a query paramater
-                    string RequestURI = "https://jsonplaceholder.typicode.com/posts/" + userMessage;
+                    string RequestURI = "https://api.apixu.com/v1/forecast.json?key=ae5e72be6f5e41fc81f161958181903&q=" + userMessage;
                     HttpResponseMessage responsemMsg = await client.GetAsync(RequestURI);
                     if (responsemMsg.IsSuccessStatusCode)
                     {
                         var apiResponse = await responsemMsg.Content.ReadAsStringAsync();
 
                         //Post the API response to bot again
-                        await context.PostAsync($"Response is {apiResponse}");
+                        // await context.PostAsync($"Response is {apiResponse}");
+                        await context.PostAsync(GetMessage(context,card,"Weather card"));
 
                     }
                 }
@@ -47,7 +50,41 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             context.Wait(MessageReceivedAsync);
         }
+        
+        /*
+       private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object>  actionResult)
+        {
+            IMessageActivity message = null;
+            var weatherCard = (AdaptiveCards.AdaptiveCard)actionResult;
+            if (weatherCard == null)
+            {
+                message = context.MakeMessage();
+                message.Text = $"I couldn't find the weather for '{context.Activity.AsMessageActivity().Text}'.  Are you sure that's a real city?";
+            }
+            else
+            {
+                message = GetMessage(context, weatherCard, "Weather card");
+            }
+
+            await context.PostAsync(message);
+        }*/
+        private IMessageActivity GetMessage(IDialogContext context, AdaptiveCards.AdaptiveCard card, string cardName)
+        {
+            var message = context.MakeMessage();
+            if (message.Attachments == null)
+                message.Attachments = new List<Attachment>();
+
+            var attachment = new Attachment()
+            {
+                Content = card,
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Name = cardName
+            };
+            message.Attachments.Add(attachment);
+            return message;
+        }
     }
+   
     /*
     [Serializable]
     public class EchoDialog : IDialog<object>
